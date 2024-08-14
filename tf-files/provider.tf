6c787441e533c34f354f9919214b583d0ff9b7aa
ManCalAzure <53532031+ManCalAzure@users.noreply.github.com>
2024-08-14 11:08:36 -0400

    Delete tf-files directory

--- tf-files/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "zedcloud" {
-    zedcloud_url = "https://zedcontrol.zededa.net" #### Pick the cluster where you will be executing these commands against.
-  # Configuration options
-    zedcloud_token = "<add your Zededa session/user token here>"
-}
--- tf-files/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-#username = ""
-#password = ""
-#zedcloud_token = ""
